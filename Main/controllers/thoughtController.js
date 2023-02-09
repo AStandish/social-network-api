059fc7866c70e7cbfd6474c7f9e93fd302efc69f
AStandish <112442942+AStandish@users.noreply.github.com>
2023-02-08 22:25:10 -0500

    first commit and added most data

---
+++ Main/controllers/thoughtController.js
@@ -0,0 +1,55 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getThought(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "There are no thoughts here" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then(() => Thought.json(Thought))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Delete a thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "There are no thoughts" })
+          : User.deleteMany({ _id: { $in: thought.User } })
+      )
+      .then(() => res.json({ message: "Thought and user deleted!" }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "There are no thoughts!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
